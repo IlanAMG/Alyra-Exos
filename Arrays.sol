2335b84b89614bacf26574ab33f0f02f3e40935a
IlanAMG <ilanamzallag.dev@gmail.com>
2021-08-21 23:02:00 +0200

    ex3 + arrays

---
+++ Arrays.sol
@@ -0,0 +1,9 @@
+// // Tableau avec une longueur fixe de 2 entiers :
+// uint[2] fixedArray;
+// // un autre tableau fixe, pouvant contenir 5 string :
+// string[5] stringArray;
+// // un Array dynamique - n'a pas de taille fixe, peut contenir n éléments :
+// uint[] dynamicArray;
+
+// Vous pouvez également créer un ensemble de structures. En utilisant la structure Person :
+// Person[] public people; // un tableau dynamique de type Person, peut contenir n "Person"
